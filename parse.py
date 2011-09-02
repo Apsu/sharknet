c30a22d6bb0cb47c230ce6a69f651185033f9d82
Evan Callicoat <diopter@gmail.com>
2011-09-02 18:26:22 -0500

    Initial commit

---
+++ parse.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python2
+
+try:
+    import json # Try newer library first
+except ImportError:
+    import simplejson # Try older next, bomb if not found
+
+from sys import stdin
+
+def parse_block(handle):
+    block_type = handle.readline().strip()
+    filter_type = handle.readline().strip().replace('<No Filter>', '').replace('Filter:', '')
+    block = [filter_type] # Block starts with filter string
+
+    handle.readline() # Skip empty line
+
+    if 'Conversations' in block_type:
+        handle.readline() # Skip headers
+    
+    for line in handle:
+        if line[0] == '=': # End of block
+            return (block_type, block) # Tuple for dict
+
+        stats = line.split() # Initial division
+        
+        # Block-specific cleanup
+        if 'Conversations' in block_type:
+            stats.remove('<->')
+        elif block_type == 'Protocol Hierarchy Statistics':
+            stats[1] = stats[1].split(':')[1]
+            stats[2] = stats[2].split(':')[1]
+            
+        block.append(stats) # Add to block
+
+# Start of program
+if __name__ == '__main__':
+    blocks = {} # Initialize stat blocks
+    
+    for line in stdin:
+        if(line[0] == '='): # Start of block
+            blocks.update([parse_block(stdin)]) # Indexed by block_type
+        else:
+            pass # Ignore anything else
+    
+    print(json.dumps(blocks))
