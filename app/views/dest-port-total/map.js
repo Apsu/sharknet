560034424ab319f96099e7ac816111ffafe2d12b
Evan Callicoat <diopter@gmail.com>
2011-09-05 05:03:36 -0500

    Initial CouchApp commit

---
+++ app/views/dest-port-total/map.js
@@ -0,0 +1,7 @@
+function(doc) {
+    if (doc.type && doc.type == "TCP Conversations") {
+	for (var e in doc.events) {
+	    emit([doc.timestamp, doc.events[e].dest_port], doc.events[e].bytes_total);
+	}
+    }
+}
