a60df48af45f14bb7182117c7beee11f47f935a9
Evan Callicoat <diopter@gmail.com>
2011-09-06 17:39:03 -0500

    Newfiles

---
+++ app/viewsbak/source-ip-port-total/map.js
@@ -0,0 +1,7 @@
+function(doc) {
+    if (doc.type && doc.type == "TCP Conversations") {
+	for (var e in doc.events) {
+	    emit(doc.timestamp.concat([doc.events[e].source_ip, doc.events[e].source_port]), doc.events[e].bytes_total);
+	}
+    }
+}
