0610a6f381581fbb2133f92999b6ef173735cdf5
Evan Callicoat <diopter@gmail.com>
2011-09-07 02:03:28 -0500

    Shark harness, averaging for duration, parameterizing

--- app/views/source-port-out/map.js
+++ app/views/source-port-out/map.js
@@ -1,7 +1,7 @@
 function(doc) {
     if (doc.type && doc.type == "TCP Conversations") {
 	for (var e in doc.events) {
-	    emit(doc.hostname.concat([doc.timestamp, doc.events[e].source_port]), doc.events[e].bytes_out);
+	    emit(doc.hostname.concat(["", doc.timestamp, doc.events[e].source_port]), doc.events[e].bytes_out);
 	}
     }
 }
--- parse.py
+++ parse.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python2
 
 import platform
-from sys import stdin
+from sys import stdin, argv
 
 from couchdb import Server
 from couchdb.mapping import datetime
 
-def parse_tcp_conversations(block_type):
+def parse_tcp_conversations(block_type, duration):
     filter_type = stdin.next().strip().replace('<No Filter>', '').replace('Filter:', '')
-    block = {'type': block_type, 'filter': filter_type, 'hostname': platform.node(),'timestamp': datetime.utcnow().utctimetuple()[0:6], 'events': []}
-    events = ['source_ip', 'source_port', 'dest_ip', 'dest_port', 'frames_in', 'bytes_in', 'frames_out', 'bytes_out', 'frames_total', 'bytes_total']
+    block = {'type': block_type,
+             'filter': filter_type,
+             'hostname': platform.node(),
+             'duration': duration,
+             'timestamp': datetime.utcnow().utctimetuple()[0:6],
+             'events': []}
+    events = ['source_ip',
+              'source_port',
+              'dest_ip',
+              'dest_port',
+              'frames_in',
+              'bytes_in',
+              'frames_out',
+              'bytes_out',
+              'frames_total',
+              'bytes_total']
 
     stdin.next() # Skip empty line
     stdin.next() # Skip headers
@@ -18,13 +32,12 @@ def parse_tcp_conversations(block_type):
         if line[0] == '=': # End of block
             return block
 
-        stats = sum([item.split(':') for item in line.split()], []) # Split and flatten
-        stats.remove('<->')
-        for i in range(len(stats)):
-            try: stats[i] = long(stats[i])
-            except ValueError: pass
-
-        block['events'].append(dict(zip(events, stats)))
+        stats = line.split() # Initial division
+        stats.remove('<->') # Decruft
+        stats = sum(map(lambda(x): x.split(':'), stats), []) # Split and flatten
+        stats = map(lambda(x): x.isdigit() and long(x) or x, stats) # Convert numbers
+        stats[5:9] = map(lambda(x): long(x / duration), stats[5:9]) # Get average
+        block['events'].append(dict(zip(events, stats))) # Zip to dict and append
 
 # Start of program
 if __name__ == '__main__':
@@ -37,7 +50,8 @@ if __name__ == '__main__':
         if(line[0] == '='): # Start of block
             block_type = stdin.next().strip()
             if 'TCP' in block_type: # TCP Conversations
-                block = parse_tcp_conversations(block_type)
-                db.create(block) # Add block to DB
+                block = parse_tcp_conversations(block_type, long(argv[1]))
+                #db.create(block) # Add block to DB
+                print(block)
         else:
             pass # Ignore anything else
