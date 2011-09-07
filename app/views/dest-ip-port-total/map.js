0074f2db9a4d2df752bd65be11341ad25a95ecb2
Evan Callicoat <diopter@gmail.com>
2011-09-07 01:14:44 -0500

    mm

--- app/views/dest-ip-port-total/map.js
+++ app/views/dest-ip-port-total/map.js
@@ -1,7 +1,7 @@
 function(doc) {
     if (doc.type && doc.type == "TCP Conversations") {
 	for (var e in doc.events) {
-	    emit(doc.timestamp.concat([doc.events[e].dest_ip, doc.events[e].dest_port]), doc.events[e].bytes_total);
+	    emit(doc.hostname.concat([doc.timestamp, doc.events[e].dest_ip, doc.events[e].dest_port]), doc.events[e].bytes_total);
 	}
     }
 }
--- app/viewsbak/dest-port-total/map.js
+++
@@ -1,7 +0,0 @@
-function(doc) {
-    if (doc.type && doc.type == "TCP Conversations") {
-	for (var e in doc.events) {
-	    emit(doc.timestamp.concat([doc.events[e].dest_port]), doc.events[e].bytes_total);
-	}
-    }
-}
