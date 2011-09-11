ed4010cae9e7239fcd58e042b7410941997a1cd9
Evan Callicoat <diopter@gmail.com>
2011-09-11 18:38:10 -0500

    Removed old views

--- oldviews/source-ip-port-in/map.js
+++
@@ -1,7 +0,0 @@
-function(doc) {
-    if (doc.type && doc.type == "TCP Conversations") {
-	for (var e in doc.events) {
-	    emit([doc.hostname].concat(doc.timestamp, doc.events[e].source_ip, doc.events[e].source_port), parseInt(doc.events[e].bytes_in / doc.duration));
-	}
-    }
-}
