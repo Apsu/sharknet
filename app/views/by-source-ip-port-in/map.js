c10a9e9927f540e6b312171a4a4fef242b488f73
Evan Callicoat <diopter@gmail.com>
2011-09-11 05:03:23 -0500

    Source views too!

---
+++ app/views/by-source-ip-port-in/map.js
@@ -0,0 +1,7 @@
+function(doc) {
+    if (doc.type && doc.type == "TCP Conversations") {
+	    for (var e in doc.events) {
+	        emit([doc.hostname].concat(doc.events[e].source_ip, doc.events[e].source_port, doc.timestamp), parseInt(doc.events[e].bytes_in / doc.duration));
+	    }
+    }
+}
