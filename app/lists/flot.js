f7f0d739896b7c2282901ea253350ec7fcea2a41
Evan Callicoat <diopter@gmail.com>
2011-09-11 23:43:13 -0500

    Listage

--- app/lists/flot.js
+++ app/lists/flot.js
@@ -1,7 +1,25 @@
 function(head, req) {
-    var row;
-    var series = [];
+    var series = {}, row, entry;
     while(row = getRow()) {
-        
+        entry = [Date.UTC.apply(this, row.key.slice(2,9)), row.value];
+        if(series[row.key[1]]) {
+            series[row.key[1]].push(entry);
+        }
+        else {
+            series[row.key[1]] = [entry];
+        }
     }
+    
+    start({
+        "headers": {
+            "Content-Type": "application/json"
+        }
+    });
+
+    var data = [], item;
+    for(item in series) {
+        data.push([{"label": item, "data": series[item]}]);
+    }
+    
+    send(toJSON(data));
 }
