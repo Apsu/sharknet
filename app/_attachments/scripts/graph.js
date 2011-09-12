7c79dd7961bb4e707b71c22c7a8bab571907ff0e
Evan Callicoat <diopter@gmail.com>
2011-09-11 22:35:12 -0500

    Flotsam and Jetsom

--- app/_attachments/scripts/graph.js
+++ app/_attachments/scripts/graph.js
@@ -1,9 +1,11 @@
 function graph () {
-    var options = {
+    var flot_options = {
         lines: { show: true },
         points: { show: true },
-        xaxis: { mode: "time", timeformat: "%y/%m/%d" }
+        xaxis: { mode: "time" } //, timeformat: "%y/%m/%d %H:%M:%S" }
     };
+    
+    var flot_data = [{label: "10.0.0.1", data: []}];
 
     var placeholder = $("#placeholder");
     var auth = {
@@ -15,20 +17,22 @@ function graph () {
     $.couch.login(auth);    
     $db = $.couch.db("db");
 
-    $db.view("sharknet/by-dest-port-total", {
+    $db.view("sharknet/by-dest-ip-total", {
         success: function(data) {
             for (i in data.rows) {
-                $("div#placeholder").append(data.rows[i].key + "&nbsp;&nbsp;");
-                $("div#placeholder").append(data.rows[i].value + "<br>");
+                //$("div#placeholder").append(data.rows[i].key + "&nbsp;&nbsp;");
+                //$("div#placeholder").append(data.rows[i].value + "<br>");
+                flot_data[0]["data"].push([Date.UTC.apply(this, data.rows[i].key.slice(2,9)), data.rows[i].value]);
             }
+            console.log(flot_data);
+            $.plot(placeholder, flot_data, flot_options);
         },
         stale: "update_after",
         group: true,
-        group_level: 7,
-        startkey: ["loki"],
-        endkey: ["loki", []]
+        group_level: 8,
+        startkey: ["loki", "10.0.0.1"],
+        endkey: ["loki", "10.0.0.75", []]
     });
 
-    //$.plot(placeholder, merge, options);
     //setTimeout(fetchData, 1000);
 };
