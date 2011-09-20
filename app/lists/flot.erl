2d52953c219e449d5ef4d326f1cdbcc595e04d92
Evan Callicoat <diopter@gmail.com>
2011-09-20 01:40:19 -0500

    ERLANG HARDER

--- app/lists/flot.erl
+++ app/lists/flot.erl
@@ -1,11 +1,43 @@
 fun(Head, {Req}) ->
 %%  Start({[{<<"headers">>,{[{<<"Content-Type">>,<<"text/plain">>}]}}]}),
-  Fun = fun({Row}, _) ->
-    {[{<<"key">>, [Hostname, DestIP, Year, Month, Day, Hour, Minute, Second]}, {<<"value">>, Value}]} = {Row},
-    Send([Hostname, ", ", DestIP]),
-    Send(<<"\n">>),
-    {ok, nil}
+  Stamp = fun(Fields) ->
+    case length(Fields) of
+      1 -> [Year] = Fields,
+        {{Year, 1, 1}, {0, 0, 0}};
+      2 -> [Year, Month] = Fields,
+        {{Year, Month, 1}, {0, 0, 0}};
+      3 -> [Year, Month, Day] = Fields,
+        {{Year, Month, Day}, {0, 0, 0}};
+      4 -> [Year, Month, Day, Hour] = Fields,
+        {{Year, Month, Day}, {Hour, 0, 0}};
+      5 -> [Year, Month, Day, Hour, Minute] = Fields,
+        {{Year, Month, Day}, {Hour, Minute, 0}};
+      6 -> [Year, Month, Day, Hour, Minute, Second] = Fields,
+        {{Year, Month, Day}, {Hour, Minute, Second}};
+      _ -> {{1970, 1, 1}, {0, 0, 0}}
+    end
   end,
-  {ok, _} = FoldRows(Fun, nil),
-  <<"">>
+
+  Epoch = fun(Gregorian) ->
+    UnixG = calendar:datetime_to_gregorian_seconds(Gregorian),
+    UnixE = calendar:datetime_to_gregorian_seconds({{1970,1,1},{0,0,0}}),
+    UnixG - UnixE
+  end,
+
+  Fold = fun({Row}, Dict) ->
+    {[{<<"key">>, Key}, {<<"value">>, Value}]} = {Row},
+    [Hostname, DestIP | Timestamp] = Key, %% group_level >= 2
+    Unix = Epoch(Stamp(Timestamp)),
+    {ok, dict:update_counter(Unix, Value, Dict)}
+  end,
+
+%%    Send({[{<<"label">>, DestIP}, {<<"series">>, integer_to_list(Epoch(DateTime))}]}),
+
+  {ok, Stats} = FoldRows(Fold, dict:new()),
+  dict:fold(
+    fun(K, V, A) ->
+      Send(integer_to_list(V)),
+      nil
+    end
+    , nil, Stats)
 end.
